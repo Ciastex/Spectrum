5ff4089f447f389a29785927fceacd24ba501040
Tomasz Cichoń <mrciastexx@gmail.com>
2019-06-10 23:52:35 +0200

    Updated assembly information, updated Distance decapsulated DLL.

--- Spectrum.Bootstrap/Properties/AssemblyInfo.cs
+++ Spectrum.Bootstrap/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ciastex")]
 [assembly: AssemblyProduct("Spectrum.Bootstrap")]
-[assembly: AssemblyCopyright("Copyright © Ciastex 2015")]
+[assembly: AssemblyCopyright("Copyright © 2019 Ciastex")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.22.5909")]
-[assembly: AssemblyFileVersion("1.0.22.5909")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
